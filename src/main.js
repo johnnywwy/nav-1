93d5239c685994755050363e47164ca208bee6c7
johnnywwy <johnnywwy@gmail.com>
2022-01-21 23:08:04 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,71 @@
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+
+window.hashMap = xObject || [
+    { logo: 'A', url: 'https://www.acfun.cn' },
+    { logo: 'B', url: 'https://bilibili.com' }
+]
+
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+            <div class="site">
+                <div class="logo">${node.logo}</div>
+                <div class="link">${simplifyUrl(node.url)}</div>
+                <div class="close">
+                    <svg class="icon">
+                        <use xlink:href="#icon-close"></use>
+                    </svg>
+                </div>
+            </div>rm
+        </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()//阻止冒泡
+            hashMap.splice(index, 1)
+            render()
+        })
+    })
+}
+render()
+
+$('.addButton').on('click', () => {
+    let url = window.prompt('请输入要添加网址')
+    console.log(url);
+    if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
+    }
+    hashMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        url: url
+
+    })
+
+    render()
+})
+window.onbeforeunload = () => {
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)
+}
+
+$(document).on('keypress', (e) => {
+    const { key } = e //const key = e.key
+    console.log(key);
+    for (i = 0; i < hashMap.length; i++) {
+        if (key === hashMap[i].logo.toLowerCase()) {
+            window.open(hashMap[i].url)
+        }
+    }
+})
